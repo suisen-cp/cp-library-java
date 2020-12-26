ce4d21a7b72c1a201103d66cac6114ce78342598
suisen-cp <suisen.13107f4@gmail.com>
2020-12-26 20:31:27 +0900

    mod template_generator/sample

--- template_generator/sample/Main.java
+++ template_generator/sample/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int n = 3;
         int nn = n * 2;
         int nnn = 3 * 3;
-        String dir = template_generator/sample;
-        String libPath = template_generator/sample/library;
+        String dir = "template_generator/sample/";
+        String libPath = "template_generator/sample//library";
     }
 }
